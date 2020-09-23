cc388bae1f5a412dc5f06d06604cc9336cff9746
ZickkyG <giovanni.hessel@fatec.sp.gov.br>
2020-09-22 21:58:26 -0300

    fifth class - course-02

--- course-02/SistemaAutenticacao.js
+++ course-02/SistemaAutenticacao.js
@@ -2,7 +2,7 @@ import { Funcionario } from './Funcionario/Funcionario.js';
 
 export class SistemaAutenticacao {
     static login(usuario, senha) {
-        if (typeof usuario.autenticar === "undefined") throw Error('Este usuário não possuí o método para checar a senha.');
+        if (!(usuario.autenticar instanceof Function)) throw Error('Este usuário não possuí o método autenticar.');
 
         return usuario.autenticar(senha);
     }
